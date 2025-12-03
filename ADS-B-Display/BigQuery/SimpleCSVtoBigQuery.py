4221216f6c07d40797f5d0c0bb4aa3551d117427
SEUNGHA HWANG <113018059+seunghahh@users.noreply.github.com>
2025-12-03 05:18:59 -0500

    BigQuery

--- ADS-B-Display/BigQuery/SimpleCSVtoBigQuery.py
+++ ADS-B-Display/BigQuery/SimpleCSVtoBigQuery.py
@@ -42,14 +42,14 @@ else:
 current_directory = os.getcwd()
 print(current_directory)
 # Set credentials
-api_key = os.path.join(global_filepath, "t-rex.json")
+api_key = os.path.join(global_filepath, "your_json_file_name")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = api_key
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
 
 # Set table_id to the ID of the table.
-table_id = "iitp-class-team-4-473114.ADSB.Flight"
+table_id = "your_table_id"
 
 job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
